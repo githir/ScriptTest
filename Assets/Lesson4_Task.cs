b9e92395cf5bda5f66b7ad8779c09a7a19285518
githir <githir@users.noreply.github.com>
2022-07-12 10:45:09 +0900

    Initial commit

---
+++ Assets/Lesson4_Task.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Lesson4 Chapret9.5 課題：配列を宣言して出力しよう
+public class Lesson4_Task : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+        Debug.Log("\n");
+        Debug.Log("＞「課題」開始");
+        
+        // 要素の個数が5の、int型の配列arrayを宣言して好きな値で初期化する
+        Debug.Log("＞要素の個数が5の、int型の配列arrayを宣言して好きな値で初期化する");
+        int[] points = { 11, 22, 33, 44, 55 };
+        Debug.Log("int[] points = { 11, 22, 33, 44, 55 };");
+        // for文を使い、配列の各要素の値を順番に表示する
+        Debug.Log("＞for文を使い、配列の各要素の値を順番に表示する");
+        for (int i = 0; i < points.Length; i++)
+        {
+            Debug.Log(points[i]);
+        }
+        // for文を使い、配列の各要素の値を逆順に表示する
+        Debug.Log("＞for文を使い、配列の各要素の値を逆順に表示する");
+        for (int i = points.Length; i > 0; i--)
+        {
+            Debug.Log(points[i - 1]);
+        }
+        Debug.Log("＞「課題」終了");
+        Debug.Log("\n");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Lesson4_Task_Ext.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int hp = 100;          // 体力
+    private int power = 25; // 攻撃力
+    private int mp = 53; // 攻撃力
+
+    // 攻撃用の関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+
+    // 魔法攻撃用の関数
+    public void Magic()
+    {
+        if (this.mp > 5)
+        {
+            this.mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+        }
+        else
+        {
+            Debug.Log("MPが足りないため、魔法が使えない。");
+        }
+    }
+
+}
+
+public class Lesson4_Task_Ext : MonoBehaviour
+{
+
+    void Start()
+    {
+
+        Debug.Log("\n");
+        Debug.Log("＞「発展課題：Bossクラスに、変数mpとMagic関数を作成してStart関数から呼び出そう」開始");
+
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+        Boss midboss = new Boss();
+
+        /*
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+
+        // 攻撃用の関数を呼び出す
+        midboss.Attack();
+        // 防御用の関数を呼び出す
+        midboss.Defence(2);
+        */
+
+        // 魔法攻撃用の関数を呼び出す
+        for (int i = 1; i < 13  ; i++)
+        {
+            Debug.Log("＞魔法攻撃" + i + "回目");
+            lastboss.Magic();
+        }
+
+        Debug.Log("＞「発展課題」終了");
+        Debug.Log("\n");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
